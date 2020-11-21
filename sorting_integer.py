d4d976b4e1125179d69b088e860247cfad14e0a7
MackRoe <elaine.music@students.makeschool.com>
2020-11-21 08:55:16 -0500

    code counting_sort method

--- sorting_integer.py
+++ sorting_integer.py
@@ -7,10 +7,28 @@ def counting_sort(numbers):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Find range of given numbers (minimum and maximum integer values)
+    min_num = min(numbers)
+    max_num = max(numbers)
+    num_range = (max_num - min_num) + 1
+    print('Number Range: ', num_range)
 
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
+    # Create list of counts with a slot for each number in input range
+    counting_list = []
+    for i in range(num_range):
+        counting_list.append(0)
+    print("New List Before Counting: ", counting_list)
+    # Loop over given numbers and increment each number's count
+    for num in numbers:
+        counting_list[num] += 1
+    print('Accumulated count list', counting_list)
     # TODO: Loop over counts and append that many numbers into output list
+    output_list = []
+    for i in range(len(counting_list)):
+        while counting_list[i] > 0:
+            output_list.append(i)
+            counting_list[i] = counting_list[i] -1
+    print('Output List: ', output_list)
+    return output_list
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
@@ -25,3 +43,7 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+    pass
+
+these_nums = [2, 1 , 2, 3, 0, 4, 3, 6]
+counting_sort(these_nums)
